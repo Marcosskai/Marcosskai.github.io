42cb042459f62be296d762f4dc7ef613dc76807c
Marcosskai <marcos.gabrielr150@gmail.com>
2024-08-13 15:09:37 -0300

    buttonLinkedin

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,23 @@
 export function App() {
-  const linkUrl = "https://wa.me/5562981958251"; 
-  const buttonText = "WhatsApp";
-  const iconPath = "/styles/icons/icon-whatsapp.png"; 
+  const linkUrlWhatsApp = "https://wa.me/5562981958251"; 
+  const buttonTextWhatsApp = "WhatsApp";
+  const iconPathWhatsApp = "/styles/icons/icon-whatsapp.png"; 
+  const linkUrlLinkedin = "https://www.linkedin.com/in/marcos-gabriel-866182303/";
+  const buttonTextLinkedin = "Linkedin"; 
+  const iconPathLinkedin = "/styles/icons/icon-linkedin.png"; 
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <a href={linkUrl} target="_blank" rel="noopener noreferrer">
+    <div className="flex justify-center items-center h-screen gap-2">
+      <a href={linkUrlWhatsApp} target="_blank" rel="noopener noreferrer">
         <button className="px-4 py-1.5 bg-green-500 text-white rounded flex items-center gap-2 hover:bg-green-700 transition-colors duration-200">
-          <img src={iconPath} alt="WhatsApp Icon" className="w-5 h-5" />
-          {buttonText}
+          <img src={iconPathWhatsApp} alt="WhatsApp Icon" className="w-5 h-5" />
+          {buttonTextWhatsApp}
+        </button>
+      </a>
+      <a href={linkUrlLinkedin} target="_blank" rel="noopener noreferrer">
+        <button className="px-6 py-1.5 bg-blue-500 text-white rounded flex items-center gap-2 hover:bg-blue-700 transition-colors duration-200">
+          <img src={iconPathLinkedin} alt="Linkedin" className="w-5 h-5" />
+          {buttonTextLinkedin}
         </button>
       </a>
     </div>
