2df6500bbdc3ce2760d73ed17fd8c3b44ec0df76
Marcosskai <marcos.gabrielr150@gmail.com>
2024-08-14 10:17:49 -0300

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -51,7 +51,7 @@ export function App() {
           <MapPin className="w-4 h-4 sm:w-5 sm:h-5 text-zinc-500" />
           Goiânia - Goiás
         </span>
-        <p className='text-zinc-500'>Esse site é responsivo</p>
+        <p className='text-zinc-500'>Criei esse site para treinar responsividade</p>
       </div>
     </main>
   );
